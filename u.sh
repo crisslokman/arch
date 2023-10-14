6e8606aa97d72aa970bbd0118c9a59722eb2e19e
crisslokman <loumamoun23@gmail.com>
2023-10-14 14:40:39 +0000

    help

---
+++ u.sh
@@ -0,0 +1,15 @@
+
+
+#! /bin/sh
+
+# this is a file to update the files inside the git repo without retyping everything each time . 
+# you only need to run this 
+
+ls 
+git add .
+git status 
+
+echo "commit type : "
+read commit
+git commit -m $commit
+git push
